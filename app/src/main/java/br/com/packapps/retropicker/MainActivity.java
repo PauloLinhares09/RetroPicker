8c6f7b540d2d010be149c42b0d3566513f09dd0d
Paulo Linhares <paulolinhares@mbp-de-macbook.home>
2018-02-20 20:48:06 -0300

    added imageView

--- app/src/main/java/br/com/packapps/retropicker/MainActivity.java
+++ app/src/main/java/br/com/packapps/retropicker/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import br.com.packapps.retropicker.callback.CallbackPicker;
@@ -12,10 +13,14 @@ import br.com.packapps.retropicker.config.Retropicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 
     public void btActionImage(View view) {
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(Bitmap bitmap, String imagePath) {
                 Toast.makeText(MainActivity.this, "bitmap: " + bitmap, Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, "imagePath: " + imagePath, Toast.LENGTH_SHORT).show();
+                imageView.setImageBitmap(bitmap);
             }
 
             @Override
